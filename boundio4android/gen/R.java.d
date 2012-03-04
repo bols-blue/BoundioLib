e846f3148fed55213b70c632c82369c71c7ee473
bols <bols-blue@lnc.jp>
2012-03-04 13:09:29 +0900

    del files

--- boundio4android/gen/R.java.d
+++
@@ -1,4 +0,0 @@
-/home/bols/eclipse/tsukuru_workspace/BoundioLib/android_boundio4j/gen/jp/lnc/java/lib/R.java \
- : /home/bols/eclipse/tsukuru_workspace/BoundioLib/android_boundio4j/res/layout/main.xml \
-/home/bols/eclipse/tsukuru_workspace/BoundioLib/android_boundio4j/res/values/strings.xml \
-/home/bols/eclipse/tsukuru_workspace/BoundioLib/android_boundio4j/AndroidManifest.xml \
