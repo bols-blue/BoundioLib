61491a8d7b621f2f96ea51749ce600f9dc8dfb63
bols <bols-blue@lnc.jp>
2012-03-04 13:31:19 +0900

    fixed error'swq

--- boundio4a/build.sh
+++ boundio4a/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 rm -rf src/jp
-cp -r ../boundio_lib/src/main/java/* ./src/
+cp -r ../boundio4j/src/main/java/* ./src/
 perl -pi -e 's/(public class)/import android.util.Log;\n\n$1/g' src/*/*/*/*/*/*.java
 perl -pi -e 's/(public class)/import android.util.Log;\n\n$1/g' src/*/*/*/*/*.java
 perl -pi -e 's/System.out.println\(/Log.d\("boundio4j",/g' src/*/*/*/*/*/*.java
 perl -pi -e 's/System.out.println\(/Log.d\("boundio4j",/g' src/*/*/*/*/*.java
 ant release
-cp bin/classes.jar android_boundio4j.jar
+cp bin/classes.jar boundio4a.jar
--- boundio4a/gen/R.java.d
+++ boundio4a/gen/R.java.d
@@ -1,4 +1,4 @@
-/home/bols/eclipse/tsukuru_workspace/BoundioLib/android_boundio4j/gen/jp/lnc/java/lib/R.java \
- : /home/bols/eclipse/tsukuru_workspace/BoundioLib/android_boundio4j/res/layout/main.xml \
-/home/bols/eclipse/tsukuru_workspace/BoundioLib/android_boundio4j/res/values/strings.xml \
-/home/bols/eclipse/tsukuru_workspace/BoundioLib/android_boundio4j/AndroidManifest.xml \
+/home/bols/eclipse/tsukuru_workspace/BoundioLib/boundio4a/gen/jp/lnc/java/lib/R.java \
+ : /home/bols/eclipse/tsukuru_workspace/BoundioLib/boundio4a/res/layout/main.xml \
+/home/bols/eclipse/tsukuru_workspace/BoundioLib/boundio4a/res/values/strings.xml \
+/home/bols/eclipse/tsukuru_workspace/BoundioLib/boundio4a/AndroidManifest.xml \
